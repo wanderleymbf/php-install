150449b448fd11dc4fe3faa100a2a658c516bf91
wanderleymbf <wanderley.macedo@sabaojua.com.br>
2024-07-01 15:09:16 -0300

    feat:script added

---
+++ installphp.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+sudo apt install php-common libapache2-mod-php php-cli
+/etc/init.d/apache2 stop
+/etc/init.d/apache2 start
+
+sudo apt install php-mysql php-curl
+
+sudo apt-get update
+sudo apt install php-xml
+sudo apt-get install php-mbstring
+
+
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+sudo mv composer.phar /usr/local/bin/composer
+exit $RESULT
